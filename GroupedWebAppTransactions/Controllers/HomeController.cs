8c59cf464ba8cdb502a494feee47d456e7cd5d03
Sethmichael01 <sethmichael1.sm@gmail.com>
2020-12-11 18:21:13 +0100

    Initial Commit

---
+++ GroupedWebAppTransactions/Controllers/HomeController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using GroupedWebAppTransactions.Models;
+using Algorand.Algod.Api;
+using Algorand.Algod.Model;
+using Algorand.Client;
+using Algorand;
+using Account = Algorand.Account;
+
+namespace GroupedWebAppTransactions.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GroupedTransactions()
+        {
+            AlgodApi algodApiInstance = new AlgodApi("https://testnet-algorand.api.purestake.io/ps1", "B3SU4KcVKi94Jap2VXkK83xx38bsv95K5UZm2lab");
+            TransactionParams transparams = null;
+            try
+            {
+                transparams = algodApiInstance.TransactionParams();
+            }
+            catch (ApiException err)
+            {
+                throw new Exception("Could not get params", err);
+            }
+            //4CLRLFJQK3WKEIHKOYTFJ5TT7W2KKQJQUXVJM4GZYLOHG5FC3C3PNF5XMQ
+            var acc1 = "5LDA2DCL4BPUBWMJXLQC47XKGPLYDY2OVBNEKBXUV6GY3NIXBSDSMTIAKA";
+            var acc2 = "RSVGYZ5653Z3X6GLFNBNM7NSR2OVQC6WRPPSOQLURI2H5R5DU35OYOQQ5A";
+            //AE47UNNWHF2XCVFF4DAB7WPHKKNL5MFEW2YQ6DTK3HYNGO4GCG32WQAA5I
+            var Key = "brain under subject brown letter select auto artwork inject display reflect clerk index prosper trick host true stable fork tone drum please loud abstract reject";
+            Account account = new Account(Key);
+            //Console.WriteLine(account.Address.ToString());
+            //Console.WriteLine($"{account.Address.ToString()} and {account.ToMnemonic()}");
+            //brain under subject brown letter select auto artwork inject display reflect clerk index prosper trick host true stable fork tone drum please loud abstract reject
+            //LLMTMNBRXUF5ARJZLYYJEJPVOGDJ2SBYOHQDWXHOPIODS6ANXBL4S6BECE
+            var accountAddress = "LLMTMNBRXUF5ARJZLYYJEJPVOGDJ2SBYOHQDWXHOPIODS6ANXBL4S6BECE";
+
+            var accountInfo = algodApiInstance.AccountInformation("LLMTMNBRXUF5ARJZLYYJEJPVOGDJ2SBYOHQDWXHOPIODS6ANXBL4S6BECE");
+
+            Console.WriteLine($"The address {accountAddress} has a Balance of: {accountInfo.Amount} microAlgos");
+            //Let's create a grouped transaction
+            var amount = Utils.AlgosToMicroalgos(1);
+
+            var tx1 = Utils.GetPaymentTransaction(new Address(accountAddress), new Address(acc1), amount, "Perfoming First Grouped Transactions", transparams);
+
+            var tx2 = Utils.GetPaymentTransaction(new Address(accountAddress), new Address(acc2), amount, "Perfoming Second Grouped Transactions", transparams);
+
+            Digest gid = TxGroup.ComputeGroupID(new Algorand.Transaction[] { tx1, tx2 });
+
+            tx1.AssignGroupID(gid);
+            tx2.AssignGroupID(gid);
+            var signedTx1 = account.SignTransaction(tx1);
+
+            var signedTx2 = account.SignTransaction(tx2);
+            try
+            {
+                List<byte> byteList = new List<byte>(Algorand.Encoder.EncodeToMsgPack(signedTx1));
+
+                byteList.AddRange(Algorand.Encoder.EncodeToMsgPack(signedTx2));
+                var acc = algodApiInstance.AccountInformation(accountAddress);
+
+                var before = $"Account 1 Balance before: {acc.Amount.ToString()}";
+
+                var id = algodApiInstance.RawTransaction(byteList.ToArray());
+
+                var wait = Utils.WaitTransactionToComplete(algodApiInstance, id.TxId);
+
+                //Console.WriteLine(wait);
+                //Console.WriteLine($"Transfer sent to Tx group with id: {id}");
+                acc = algodApiInstance.AccountInformation(accountAddress.ToString());
+            }
+            catch (ApiException err)
+            {
+                //Console.WriteLine($"Exception when calling algodrawtransactions: {err.Message}");
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
